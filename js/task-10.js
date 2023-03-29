8a1d4160b23a20d1cf36e7f76aa520dd6cd8b47e
Symeon Melnychuk <m.simeon998@gmail.com>
2023-03-29 13:41:10 +0300

    3 здача

--- js/task-10.js
+++ js/task-10.js
@@ -6,12 +6,25 @@ function getRandomHexColor() {
 
 const buttonCreate = document.querySelector('button[data-create]');
 const buttonDestroy = document.querySelector('button[data-destroy]');
-
-
+const number = document.querySelectorAll('input')
+const box = document.querySelector('#boxes')
+console.dir(number)
 
 buttonCreate.addEventListener('click', onCreate);
 // buttonDestroy.addEventListener('click', onDestory)
+let amount = Number(number.value) /// values
+
+function createBoxes(amount) {
+ for (let index = 0; index < amount; index+=1) {
+  const div = `<div class = box></div>`;
+  box.insertAdjacentHTML('afterbegin', div)
+ }
+  
+}
 
 function onCreate(){
-  getRandomHexColor()
+  createBoxes()
 }
+// console.log(amount)
+
+
